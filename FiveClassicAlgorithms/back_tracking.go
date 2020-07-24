3797981ffc0ae0ef60ff7eb28a9484bd252d5b0c
xujiacan <xujiacan@juzix.net>
2020-07-24 10:17:46 +0800

    '完成8王后的回溯算法'
    Signed-off-by: xujiacan <xujiacan@juzix.net>

--- FiveClassicAlgorithms/back_tracking.go
+++ FiveClassicAlgorithms/back_tracking.go
@@ -5,7 +5,7 @@ import "fmt"
 // https://zhuanlan.zhihu.com/p/93530380
 func main() {
 
-	findGraph(4)
+	findGraph(5)
 }
 
 // 回溯算法 (八 王后问题)
@@ -47,51 +47,50 @@ func findGraph(n int) { // n 为 n位王后在 n * n 的棋盘上的布局
 	printBoard(board)
 }
 
+
+var printCount int
 func printBoard (board [][]string) {
-	//fmt.Println(board)
+	printCount ++
+	fmt.Println("第 ", printCount, " 次, 打印棋盘 ...")
 	for row := 0; row < len(board); row++ {
 		fmt.Println(board[row])
 	}
 }
 
 // todo 回溯
-func backtrack(board [][]string, row int) {
-
-	fmt.Println("进来操作第 ", row, " 行.......")
+func backtrack(board [][]string, row int) bool {
 
 	// 路径：board 中小于 row 的那些行都已经成功放置了皇后
 	// 选择列表：第 row 行的所有列都是放置皇后的选择
 	// 结束条件：row 超过 board 的最后一行
 
 	// todo 触发结束条件
-	if row == len(board) - 1 {
-		fmt.Println("结束遍历, 第 ", row, " 行.......")
-		return // todo 返回最后棋盘的布局
+	if row == len(board) {
+		return true // todo 返回最后棋盘的布局
 	}
 
 	// todo 在当前行, 遍历各个 小格子
 	colNum := len(board[row])
 
 	for col := 0; col < colNum; col++ {
-		fmt.Println("开始遍历第 ", row, " 行的, 列数, 第 ", col, " 列")
 		// todo 排除不合法选择
 		if !isValid(board, row, col) {
 			continue
 		}
 		// todo 做选择,   当前空格放置王后 `Q`
 		board[row][col] = "Q"
-		fmt.Println("【开始放】, 第 ", row, " 行的, 列数, 第 ", col, " 列 的 Q")
-
 
 		// todo 递归 进入下一行决策
-		backtrack(board, row+1)
+		if over := backtrack(board, row+1); over {
+			return true
+		}
 
 		// todo 撤销选择, 如果 递归能够出来, 说明已经走到头了, 需要 退出来 回溯上一步
 		//
 		// 还原该步 棋盘
 		board[row][col] = "."
-		fmt.Println("【清空】, 第 ", row, " 行的, 列数, 第 ", col, " 列 的 Q")
 	}
+	return false
 }
 
 /* 是否可以在 board[row][col] 放置皇后？ */
@@ -103,9 +102,6 @@ func isValid(board [][]string, row, col int) bool {
 	// 检查 ·列· 是否有皇后互相冲突
 	for i := 0; i < rowNum; i++ {
 		if board[i][col] == "Q" {
-
-			fmt.Println( "row:", row, "当前row:", i, "col:", col, "发现【当前列】已经有 Q")
-			printBoard(board)
 			return false
 		}
 	}
@@ -116,8 +112,6 @@ func isValid(board [][]string, row, col int) bool {
 	j := col + 1
 	for i >= 0 && j < rowNum {
 		if board[i][j] == "Q" {
-			fmt.Println("row:", row, "当前row:", i, "col:", col, "发现【右上方】已经有 Q")
-			printBoard(board)
 			return false
 		}
 		i--
@@ -129,9 +123,6 @@ func isValid(board [][]string, row, col int) bool {
 	// 检查左上方是否有皇后互相冲突
 	for i >= 0 && j >= 0 {
 		if board[i][j] == "Q" {
-
-			fmt.Println("row:", row, "当前row:", i, "col:", col, "发现 【左上方】已经有 Q")
-			printBoard(board)
 			return false
 		}
 		i--
